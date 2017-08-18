d72016f99c153720836eb95fbfe6a14f40131796
Alex Tereshkov <ter.alexanderandr@gmail.com>
2017-08-18 13:34:45 -0700

    Initial commit with podspec

---
+++ EXPLogger/EXPLogger.h
@@ -0,0 +1,19 @@
+//
+//  EXPLogger.h
+//  EXPLogger
+//
+//  Created by Alex Tereshkov on 8/18/17.
+//  Copyright © 2017 Alex Tereshkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EXPLogger.
+FOUNDATION_EXPORT double EXPLoggerVersionNumber;
+
+//! Project version string for EXPLogger.
+FOUNDATION_EXPORT const unsigned char EXPLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EXPLogger/PublicHeader.h>
+
+
---
+++ EXPLogger/LogManager.swift
@@ -0,0 +1,17 @@
+//
+//  LogManager.swift
+//  EXPLogger
+//
+//  Created by Alex Tereshkov on 8/18/17.
+//  Copyright © 2017 Alex Tereshkov. All rights reserved.
+//
+
+import Foundation
+
+class LogManager: NSObject {
+    
+    public func getMyTestLog(value: String) -> String {
+        return "Hello, \(value)"
+    }
+    
+}
